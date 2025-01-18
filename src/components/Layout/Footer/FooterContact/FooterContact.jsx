0fdfd99eff1e7eb6fca31eac4cebeadf35ca0fd1
Islomjonsdev <islomjon@immigrateable.com>
2025-01-18 13:56:07 +0500

    fixed footer bug

--- src/components/Layout/Footer/FooterContact/FooterContact.jsx
+++ src/components/Layout/Footer/FooterContact/FooterContact.jsx
@@ -6,13 +6,13 @@ const FooterContact = () => {
   return (
     <ul className="footer_contact">
       <li>
-        <a href="tel:+998998107090">+99899 810-70-90</a>
+        <a className="footer_contact_link" href="tel:+998998107090">+99899 810-70-90</a>
       </li>
       <li>
-        <a href="tel:+998998147090">+99899 814-70-90</a>
+        <a className="footer_contact_link" href="tel:+998998147090">+99899 814-70-90</a>
       </li>
       <li>
-        <a href="tel:+998958157090">+99895 815-70-90</a>
+        <a className="footer_contact_link" href="tel:+998958157090">+99895 815-70-90</a>
       </li>
       <li>
         <Links title={"Связаться с нами"} />
